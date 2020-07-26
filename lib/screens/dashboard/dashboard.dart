7c89373caf40f0b2774fb158785b7baca7499e66
Fernando Simoes <fernando.simoes@dextra-sw.com>
2020-07-26 20:55:08 -0300

    gerado componente de menu

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -32,76 +32,77 @@ class _DashboardState extends State<Dashboard> {
             scrollDirection: Axis.horizontal,
             child: Row(
               children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Material(
-                    color: Theme.of(context).primaryColor,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => ListContatos()));
-                      },
-                      child: Container(
-                        width: 180,
-                        height: 110,
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Icon(
-                              Icons.people,
-                              size: 24,
-                              color: Colors.white,
-                            ),
-                            Text('Contatos ',
-                                style: TextStyle(
-                                  fontSize: 24.0,
-                                  color: Colors.white,
-                                ))
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                _menuItem(
+                    'Contatos',
+                    Icons.contacts,
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => ListContatos()));
+                    },
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Material(
-                    color: Theme.of(context).primaryColor,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => ListaTransferencias()));
-                      },
-                      child: Container(
-                        width: 180,
-                        height: 110,
-                        padding: EdgeInsets.all(8),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            Icon(
-                              Icons.attach_money,
-                              size: 24,
-                              color: Colors.white,
-                            ),
-                            Text('Transferencias ',
-                                style: TextStyle(
-                                  fontSize: 24.0,
-                                  color: Colors.white,
-                                ))
-                          ],
-                        ),
-                      ),
-                    ),
-                  ),
+                _menuItem(
+                  'Transferencias Realizadas', Icons.attach_money,
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (context) => ListaTransferencias()));
+                  },
                 ),
+//                _menuItem(
+//                  'Transferencias', Icons.attach_money,
+//                    onTap: () {
+//                      Navigator.of(context).push(MaterialPageRoute(
+//                      builder: (context) => ListaTransferencias()));
+//                    },
+//                ),
               ],
             ),
           )
         ],
+      ),
+    );
+  }
+}
+
+class _menuItem extends StatelessWidget {
+
+  final String menuName;
+  final IconData menuIcon;
+  final Function onTap;
+  _menuItem(this.menuName, this.menuIcon, {@required this.onTap});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Material(
+        color: Theme.of(context).primaryColor,
+        child: InkWell(
+          onTap: () {
+            this.onTap();
+
+          },
+          child: Container(
+            width: 180,
+            height: 110,
+            padding: EdgeInsets.all(8),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  this.menuIcon,
+                  size: 24,
+                  color: Colors.white,
+                ),
+                Text(this.menuName,
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      color: Colors.white,
+                    ))
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
